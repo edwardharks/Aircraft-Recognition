82c364a92bce79e557ba2dcfd7ed4360f29a5224
Edward <edwardharks@yahoo.co.uk>
2017-04-22 11:55:38 +0100

    Adds a warning when images load to tell me if i've copied the size incorrectly

--- app/src/main/kotlin/com/edwardharker/aircraftrecognition/ui/ImageViewGlideExtension.kt
+++ app/src/main/kotlin/com/edwardharker/aircraftrecognition/ui/ImageViewGlideExtension.kt
@@ -2,6 +2,7 @@ package com.edwardharker.aircraftrecognition.ui
 
 import android.graphics.drawable.ColorDrawable
 import android.support.v4.content.ContextCompat.getColor
+import android.util.Log
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -33,18 +34,23 @@ fun ImageView.loadImage(image: Image, imageLoadedListener: (() -> Unit)? = null)
             .placeholder(ColorDrawable(getColor(context, R.color.colorPrimaryLight)))
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .dontAnimate()
-            .listener(Listener(imageLoadedListener))
+            .listener(Listener(image, imageLoadedListener))
             .into(this)
     requestLayout()
 }
 
-private class Listener(private val imageLoadedListener: (() -> Unit)?) : RequestListener<String, GlideDrawable> {
+private class Listener(val image: Image, val imageLoadedListener: (() -> Unit)?) : RequestListener<String, GlideDrawable> {
     override fun onException(e: Exception?, model: String?, target: Target<GlideDrawable>?, isFirstResource: Boolean): Boolean {
         imageLoadedListener?.invoke()
         return false
     }
 
     override fun onResourceReady(resource: GlideDrawable?, model: String?, target: Target<GlideDrawable>?, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean {
+        resource?.let {
+            if (it.intrinsicWidth != image.width || it.intrinsicHeight != image.height) {
+                Log.w("ImageLoadListener", "Image downloaded from the internet does not match the expected size. Url: ${image.url}")
+            }
+        }
         imageLoadedListener?.invoke()
         return false
     }
