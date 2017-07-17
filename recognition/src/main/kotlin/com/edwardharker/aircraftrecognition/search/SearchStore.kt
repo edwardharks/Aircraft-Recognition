ad9a918e6d08639afde0b165d59563d2337732d8
Edward <edwardharks@yahoo.co.uk>
2017-07-17 20:32:31 +0100

    Small refactor to SearchStore

--- recognition/src/main/kotlin/com/edwardharker/aircraftrecognition/search/SearchStore.kt
+++ recognition/src/main/kotlin/com/edwardharker/aircraftrecognition/search/SearchStore.kt
@@ -7,13 +7,13 @@ class SearchStore(private val search: (action: QueryChangedAction) -> Observable
                   private val reducer: (oldState: SearchState, action: SearchAction) -> SearchState) {
 
     fun observe(actions: Observable<SearchAction>): Observable<SearchState> =
-            actions.compose(this::actions)
-                    .scan(SearchState.empty(), reducer)
+            actions.compose {
+                actions.publish {
+                    it.ofType(QueryChangedAction::class.java).compose(this::queryChangedAction)
+                }
+            }.scan(SearchState.empty(), reducer)
 
-    private fun actions(actions: Observable<SearchAction>): Observable<SearchAction> =
-            // if there were more actions, use merge here
-            actions.publish { it.ofType(QueryChangedAction::class.java).compose(this::searchQuery) }
 
-    private fun searchQuery(actions: Observable<QueryChangedAction>) = actions.flatMap(search)
+    private fun queryChangedAction(actions: Observable<QueryChangedAction>) = actions.flatMap(search)
 
 }
